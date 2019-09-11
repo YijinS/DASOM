9b22bc7e3eee36de535cbb6c562d458abcf8b71f
sojian <dothewillof@naver.com>
2019-09-11 19:12:35 +0900

    2019.09.11 화면

--- src/main/java/com/dasomcompany/myapp/HomeController.java
+++ src/main/java/com/dasomcompany/myapp/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -36,4 +36,18 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	public String main(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "main";
+	}
 }
